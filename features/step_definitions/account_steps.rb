d98c95750aaa8908406f5985c3967d52be068b6e
Your Name <jp_ca@live.com>
2011-10-26 09:42:56 -0700

    refactored code & added useful comments

--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -1,15 +1,20 @@
 Given /^I have an account$/ do
-  @account = Account.new
+  @account = Account.new  # here @account is a new object of Account
   end
 
-Given /^it has a balance of (\d+)$/ do |amount2|
-  @account.balance = amount2   #pending # express the regexp above with the code you wish you had
+Given /^it has a balance of (\d+)$/ do |amount_received|
+  @account.balance = amount_received   #pending # express the regexp above with the code you wish you had
 end
 
-When /^I take out (\d+)$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+When /^I take out (\d+)$/ do |amount_to_take_out|
+  @account.balance =  @account.balance - amount_to_take_out  # error occurred because you’re not storing 
+                                                             # the balance as a Fixnum but as a String. 
 end
 
 Then /^my balance should be (\d+)$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
+
+
+
+
--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,15 @@
 class Account 
-  def balance=(amount1)  #balance is method
-    @balance = amount1   # amount is object & is passed in as the single argument for this method.
-                        # you set the @balance instance variable to that value.
+  def balance=(amount_object) #balance is setter method
+    @balance = amount_object  # amount_object is object & is passed in as the single argument for this method.
+    # you set the @balance instance variable to that value.
   end
-  
+
+  def balance  #getter
+    @balance 
+  end
+
 end
+
+
+
+

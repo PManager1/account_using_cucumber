5800462066b4966d01c2224ce0b55ac476862fbc
Your Name <jp_ca@live.com>
2011-10-26 08:53:05 -0700

    first commit

---
+++ lib/account.rb
@@ -0,0 +1,7 @@
+class Account 
+  def balance=(amount1)  #balance is method
+    @balance = amount1   # amount is object & is passed in as the single argument for this method.
+                        # you set the @balance instance variable to that value.
+  end
+  
+end
